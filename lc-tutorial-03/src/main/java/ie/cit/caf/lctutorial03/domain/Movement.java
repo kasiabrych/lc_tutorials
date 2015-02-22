fed85284002fa6503be692facc0e9788d009b581
kasiabrych <kasiabrych.kb@gmail.com>
2015-02-22 12:43:12 +0000

    Tutorial 3 version before exercise

---
+++ lc-tutorial-03/src/main/java/ie/cit/caf/lctutorial03/domain/Movement.java
@@ -0,0 +1,29 @@
+package ie.cit.caf.lctutorial03.domain;
+
+public class Movement {
+
+	private int id;
+	
+	private String name;
+
+	@Override
+	public String toString() {
+		return "Movement [id=" + id + ", name=" + name + "]";
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
